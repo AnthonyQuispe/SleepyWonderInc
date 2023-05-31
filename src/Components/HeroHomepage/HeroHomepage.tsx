a08b5abce329e9c8a828fcd20d06735122fa510f
“Anthony <Anthony.s.quispe@gmail.com>
2023-05-31 14:02:04 -0400

    Completed hero component for homepage

--- src/Components/HeroHomepage/HeroHomepage.tsx
+++ src/Components/HeroHomepage/HeroHomepage.tsx
@@ -1,37 +1,56 @@
 import "./HeroHomepage.scss";
 import Image from "next/image";
 
-export default function HeroHomepage() {
+export default function HeroHomepage({ values }) {
+  const [
+    heroBackgroundImage,
+    heroHeading,
+    heroHeadingAlt,
+    heroText,
+    imageSrc,
+    imageAlt,
+  ] = values || [
+    "/GameBackground.png",
+    "ChronoBlade",
+    ": Eternia's Awakening",
+    "In 'ChronoBlade: Eternia's Awakening,' embark on an epic journey through a vast and immersive anime-inspired world. As a fearless warrior, chosen by destiny, you must harness the power of time itself to restore balance and protect Eternia from the encroaching darkness.",
+    "/HeroCharacter.png",
+    "hero character",
+    "100",
+    "100",
+  ];
+
   return (
-    <div className="hero">
-      <div className="hero-texts__container">
-        <div className="hero-heading__container">
-          <h1 className="hero-heading">
-            ChronoBlade
-            <span className="hero-heading__alt">
-              : Eternia&apos;s Awakening{" "}
-            </span>
-          </h1>
+    <div className="hero-background">
+      <Image
+        src={heroBackgroundImage}
+        alt={imageAlt}
+        width={1000}
+        height={1000}
+        className="hero-background--image"
+      />
+      <div className="hero">
+        <div className="hero-texts__container">
+          <div className="hero-heading__container">
+            <h1 className="hero-heading">
+              {heroHeading}
+              <span className="hero-heading__alt">{heroHeadingAlt}</span>
+            </h1>
+          </div>
+          <div className="hero-text__container">
+            <p className="hero-text">{heroText}</p>
+          </div>
+          <button className="hero-preview"></button>
         </div>
-        <div className="hero-text__container">
-          <p className="hero-text">
-            In &quot;ChronoBlade: Eternia&apos;s Awakening,&quot; embark on an
-            epic journey through a vast and immersive anime-inspired world. As a
-            fearless warrior, chosen by destiny, you must harness the power of
-            time itself to restore balance and protect Eternia from the
-            encroaching darkness.
-          </p>
+        <div className="hero-character__container">
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            width={100}
+            height={100}
+            className="hero-character"
+          />
         </div>
-        <button className="hero-preview"></button>
-      </div>
-      <div className="hero-character__container">
-        <Image
-          src="/HeroCharacter.png"
-          alt="hero character"
-          width="100"
-          height="100"
-          className="hero-character"
-        />
       </div>
     </div>
   );
